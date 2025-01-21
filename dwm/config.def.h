10a9dddf521fc25aa7c2b9cc6893b7bc605b8523
xunilesu <xunilesu@gmail.com>
2025-01-20 19:03:11 -0800

    removed xrdb patch from dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -100,7 +100,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_r,     xrdb,            {.v = NULL } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
